4f062b91a75c51a882fe2d03adf9736a131713e0
AWajid-S <wajidsalam7@gmail.com>
2026-05-02 13:17:14 +0100

    fix greeting log syntax

--- dist/index.js
+++ dist/index.js
@@ -26839,7 +26839,7 @@ const core = __nccwpck_require__(7484)
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello', ${nameToGreet}!`);
+    console.log(`Hello, ${nameToGreet}!`);
     core.setOutput('time', new Date().toTimeString());
 
 }   catch (error){
